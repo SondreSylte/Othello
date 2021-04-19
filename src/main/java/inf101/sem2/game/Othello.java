b3b7d5382b42bf11f59142c60bf7922dd7db4da3
sondresylte <sondre.sylte@student.uib.no>
2021-04-19 16:35:09 +0200

    Startet på Othello klassen. Første commit og push.

---
+++ src/main/java/inf101/sem2/game/Othello.java
@@ -0,0 +1,51 @@
+package inf101.sem2.game;
+
+
+import inf101.grid.Location;
+import inf101.sem2.player.Player;
+
+public class Othello extends Game {
+
+    public Othello(Graphics graphics, Player p1, Player p2) {
+        this(graphics);
+        players.add(p1);
+        players.add(p2);
+
+        board.set(new Location(3,4), p1);
+        board.set(new Location(4,3),p1);
+        board.set(new Location(3,3), p2);
+        board.set(new Location(4,4),p2);
+
+
+    }
+
+    public Othello(Graphics graphics) {
+        super(new GameBoard(8,8),graphics);
+    }
+
+    public Othello(Graphics graphics, Iterable<Player> players) {
+        super(new GameBoard(8, 8), graphics, players);
+    }
+
+
+
+    @Override
+    public Game copy() {
+        return null;
+    }
+
+    @Override
+    public boolean isWinner(Player player) {
+        return false;
+    }
+
+    @Override
+    public boolean gameOver() {
+        return false;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+}
